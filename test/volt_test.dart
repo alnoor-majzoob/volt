927a932c86698289c1867bfa7b5b230621639d77
Alnoor Majzoob <alnoormajzoob2026@gmail.com>
2025-09-25 13:15:08 +0200

    Update volt_test.dart

--- test/volt_test.dart
+++ test/volt_test.dart
@@ -1,16 +1,1 @@
-import 'package:volt/volt.dart';
-import 'package:test/test.dart';
 
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
